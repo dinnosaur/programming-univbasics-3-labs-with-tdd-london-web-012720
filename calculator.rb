147cb6bcc01fbaa68d6e18f1d3fe06a41317823b
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-18 01:41:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,8 @@ first_number = 12
 second_number =4
 sum = first_number + second_number
 difference= first_number - second_number
+product = first_number *second_number
+quotient = first_number/second_number
+
 
 
